8054ec03016ec194c7834ab7b833f13c64650b24
Jackriip <97582790+Jackriip@users.noreply.github.com>
2026-09-16 22:20:43 -0700

    Release Voidworks 0.5.19 stable

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,7 +4,7 @@ const Undefined = "undefined"
 
 var (
 	Title    = "Voidworks"
-	Version  = "0.5.18"
+	Version  = "0.5.19"
 	Revision = Undefined
 	GitHub   = "https://github.com/voidcrew/Voidworks"
 	Support  = "https://ko-fi.com/spair"
