b4798cae083721fb41091b4e30451d661bbc32d6
Jackriip <97582790+Jackriip@users.noreply.github.com>
2026-09-17 09:10:35 -0700

    Release Voidworks 0.5.20 stable

--- BUILD-WINDOWS.ps1
+++ BUILD-WINDOWS.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [ValidatePattern('^\d+\.\d+\.\d+(-[0-9A-Za-z]+([.-][0-9A-Za-z]+)*)?$')][string]$Version = '0.5.19',
+    [ValidatePattern('^\d+\.\d+\.\d+(-[0-9A-Za-z]+([.-][0-9A-Za-z]+)*)?$')][string]$Version = '0.5.20',
     [string]$Revision = 'source',
     [switch]$Test
 )
