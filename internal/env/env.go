4a41e8df657cf9b41b1256501bd9cffef8977eae
Jackriip <97582790+Jackriip@users.noreply.github.com>
2026-09-18 00:51:19 -0700

    Release Voidworks 0.5.28-beta.2

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,7 +4,7 @@ const Undefined = "undefined"
 
 var (
 	Title    = "Voidworks"
-	Version  = "0.5.28-beta.1"
+	Version  = "0.5.28-beta.2"
 	Revision = Undefined
 	GitHub   = "https://github.com/voidcrew/Voidworks"
 	Support  = "https://ko-fi.com/spair"
