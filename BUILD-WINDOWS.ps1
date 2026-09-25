2aa7fbdf625716236c10b677df4be0ad35a68a9c
Jackriip <97582790+Jackriip@users.noreply.github.com>
2026-09-25 13:21:20 -0700

    Release Voidworks 0.5.35

--- BUILD-WINDOWS.ps1
+++ BUILD-WINDOWS.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [ValidatePattern('^\d+\.\d+\.\d+(-[0-9A-Za-z]+([.-][0-9A-Za-z]+)*)?$')][string]$Version = '0.5.34',
+    [ValidatePattern('^\d+\.\d+\.\d+(-[0-9A-Za-z]+([.-][0-9A-Za-z]+)*)?$')][string]$Version = '0.5.35',
     [string]$Revision = 'source',
     [switch]$Test
 )
--- internal/env/env.go
+++ internal/env/env.go
@@ -4,7 +4,7 @@ const Undefined = "undefined"
 
 var (
 	Title    = "Voidworks"
-	Version  = "0.5.34"
+	Version  = "0.5.35"
 	Revision = Undefined
 	GitHub   = "https://github.com/voidcrew/Voidworks"
 	Support  = "https://ko-fi.com/spair"
